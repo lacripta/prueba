55b52471df5bc2ca05574b922b1d279826418f48
Jose Bocanegra <joseybv@gmail.com>
2016-10-09 10:59:40 -0500

    se implmento la conexion utilizando el Sql2o que ofrece mayor agilidad y velocidad que el dbutils

---
+++ src/main/java/datos/DB.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.sql2o.Sql2o;
+
+/**
+ *
+ * @author lacripta
+ */
+public class DB {
+
+    public static final Sql2o ELIBOM;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            ELIBOM = new Sql2o("jdbc:mysql://localhost:3306/elibom", "root", "910320");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("NO SE PUEDE ESTABLECER LA CONEXIONA LA BASE DE DATOS");
+        }
+    }
+
+}
